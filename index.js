2ac55c519e14d49199a207128d5d0bfc4085b03d
b0nn133 <bonnieroxtigers@yandex.ru>
2026-02-08 13:30:08 +0300

    secret.secret.secret.secret-TRUE-FALSE-TRUE

---
+++ index.js
@@ -0,0 +1,97 @@
+const CODE=`
+<html>
+    <head>
+        <style>
+            body {
+                background-color: black;
+                color: red;
+                font-family: 'Courier New', Courier, monospace;
+            }
+
+            button, input {
+                background-color: black;
+                border-color: red;
+                color: red;
+                font-family: 'Courier New', Courier, monospace;
+                transition: 0.1s;
+            }
+
+            input:focus {
+                outline: red;
+            }
+
+            button:active{
+                scale:90%
+            }
+
+            #vJVQw {
+  position: relative;
+  width: 100%;
+  height: auto;
+}
+#overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  pointer-events: auto; /* to allow interaction */
+  color: white;
+  /* additional styling */
+}
+
+        </style>
+        <script>
+            console.warn("GOOD LUCK.");
+            const ___=["G00D 7UCK.","Y0U'77 W0N'T G3T 1T."];
+            const ____="";
+            document.addEventListener("DOMContentLoaded", ()=>{
+                const lol=document.getElementById("blah");
+                lol.addEventListener("click",()=>{
+                    document.getElementById("request").hidden=true;
+                    document.getElementById("main").hidden=false;
+                    const _______=document.getElementById("vJVQw")
+
+                const _=document.getElementById("bJCVQu");
+                ___.forEach((___)=>{setTimeout(()=>{_.textContent=___}, 1000)});
+                document.getElementById("bla").addEventListener("click", () => {
+                console.log(document.getElementById("FJQW").value);
+                if(document.getElementById("FJQW").value!==____){
+                    _.textContent = "TH4T. W4S. 4N. 1NC0RR3CT. 1NPUT.";
+                    setTimeout(() => {
+                        ___.forEach((___)=>{setTimeout(()=>{_.textContent=___}, 1000)});
+                    }, 1000)
+                } else {
+                    alert("ARE YOU SURE YOU WANNA SEE IT?");
+                    alert("HERE IS THE SECRET...");
+                    alert("https://60nn13.is-a.dev/somethingverysecret/ns.html");
+                    window.close();
+                }
+            });
+                });
+            });
+        </script>
+    </head>
+    <body>
+        <center>
+            <div id="request">
+                <h1>SomethingVerySecret</h1>
+                <h2>Click the button below to expose the secret.</h2>
+                <button id="blah">Expose the secret!</button>
+            </div>
+            <div id="main" hidden="true">
+                <video id="vJVQw" controls>
+                </video>
+                <div id="overlay">
+                    <h1>HI!</h1>
+                <p>To finally expose the secret click the button below</p>
+                <p id="bJCVQu"></p>
+                <input type="text" id="FJQW" value="" hidden="true" placeholder="P4SSW0RD">
+                <button id="bla">EXPOSE THE SECRET</button>
+                </div>
+            </div>
+        </center>
+    </body>
+</html>
+`
+document.write(CODE);
---
+++ index.js.js
@@ -0,0 +1,20 @@
+for (let i=0;i<1300;i++) {
+    const button = document.createElement("button");
+    button.textContent = i;
+    document.body.appendChild(button);
+}
+for (let i=0;i<1300;i++) {
+    const button = document.createElement("button");
+    button.textContent = i;
+    document.body.appendChild(button);
+}
+for (let i=0;i<1300;i++) {
+    const button = document.createElement("button");
+    button.textContent = i;
+    document.body.appendChild(button);
+}
+for (let i=0;i<1300;i++) {
+    const button = document.createElement("button");
+    button.textContent = i;
+    document.body.appendChild(button);
+}
