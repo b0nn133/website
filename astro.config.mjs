f3e93bcab09b61c9214838950828e652adec3ac1
bonnie <130217662+b0nn133@users.noreply.github.com>
2026-05-19 20:56:30 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build
 export default defineConfig({
-  site: 'https://60nn13.is-a.dev',
-  redirects: {
-    '/[...slug]': 'https://boni.surge.sh/[slug]'
-  }
+  site: 'https://60nn13.is-a.dev'
 });
